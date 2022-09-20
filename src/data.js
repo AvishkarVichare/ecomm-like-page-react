e52ba8e15d9a373ffd3c6e3de0b7574468f6fd6e
Avishkar Vichare <avie9500@gmail.com>
2022-09-20 23:26:48 +0530

    added products

--- src/data.js
+++ src/data.js
@@ -23,6 +23,55 @@ const data = [
         price:10000,
         img:'https://media.istockphoto.com/photos/plain-red-tee-shirt-isolated-on-white-background-picture-id471188329?k=20&m=471188329&s=170667a&w=0&h=SAT6RFEpRKdLqTH9dqmXr-OEeDPLl4i1gmQZLupm-ro='
     },
+    {
+        name:'Olive green Cargo',
+        type:'pants',
+        price:1000,
+        img:'https://assets.ajio.com/medias/sys_master/root/20220818/lSh7/62fde20caeb269176182b7d0/-473Wx593H-461394178-green-MODEL.jpg'
+    },
+    {
+        name:'Mens formal pant',
+        type:'pants',
+        price:888,
+        img:'https://5.imimg.com/data5/RS/YS/RU/SELLER-80232528/mens-formal-pants-250x250.jpg'
+    },
+    
+    {
+        name:'Mens Oversized t-shirt',
+        type:'tshirts',
+        price:600,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3qeKL8xvJd34ZW0I-Gs_c80KEdQWI28IP9A&usqp=CAU'
+    },
+    {
+        name:'Naruto gara Oversized t-shirt',
+        type:'tshirts',
+        price:600,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3qeKL8xvJd34ZW0I-Gs_c80KEdQWI28IP9A&usqp=CAU'
+    },
+    {
+        name:'Itachi Oversized t-shirt',
+        type:'tshirts',
+        price:900,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3qeKL8xvJd34ZW0I-Gs_c80KEdQWI28IP9A&usqp=CAU'
+    },
+    {
+        name:'Jeans Men',
+        type:'pants',
+        price:1900,
+        img:'https://images.bewakoof.com/t640/men-s-slim-fit-solid-front-patch-stretch-stylish-new-trends-blue-denim-jeans-344944-1655931751-1.jpg'
+    },
+    {
+        name:'Slit fit chinos Men',
+        type:'pants',
+        price:2900,
+        img:'https://assets.ajio.com/medias/sys_master/root/h50/hfd/12277050081310/-1117Wx1400H-440717974-stone-MODEL.jpg'
+    },
+    {
+        name:'Army cargos',
+        type:'pants',
+        price:2900,
+        img:'https://5.imimg.com/data5/SELLER/Default/2020/9/JE/VW/EY/45233305/men-cotton-military-cargo-pants-camouflage-army-trousers-1000x1000.jpg'
+    },
     
 ];
 
